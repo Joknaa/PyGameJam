157d5490b0f70d56284f7e078f84789fa590d765
Joknaa <54961980+Joknaa@users.noreply.github.com>
2021-04-03 00:43:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PyGameJam
+ 
---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+
+
+class Player:
+    def __init__(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def get_position(self):
+        pass
+
+    def move(self):
+        pass
+
+    def handle_keys(self):
+        pass
+
+    def reset(self):
+        pass
+
+
+
+
+LEFT = (-1, 0)
+RIGHT = (1, 0)
